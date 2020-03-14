a4b130491a7db9433473a495205d8aae62bbb678
Lucas Fredin <ii.fredin@gmail.com>
2020-03-14 22:44:29 +0100

    Updates

---
+++ precache-manifest.768e27569b9384420ceb9a9da5397d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23de143e2eb33d39522940dc771167f",
+    "url": "/PuzzleGameDeluxe/index.html"
+  },
+  {
+    "revision": "5e2b3470a5a1f7878aa4",
+    "url": "/PuzzleGameDeluxe/static/css/main.fcdb4091.chunk.css"
+  },
+  {
+    "revision": "a60b1f80d2b4a45c8ac5",
+    "url": "/PuzzleGameDeluxe/static/js/2.28052eb9.chunk.js"
+  },
+  {
+    "revision": "726155764f20943c0959bb3e57816eb6",
+    "url": "/PuzzleGameDeluxe/static/js/2.28052eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2b3470a5a1f7878aa4",
+    "url": "/PuzzleGameDeluxe/static/js/main.740c07af.chunk.js"
+  },
+  {
+    "revision": "ba6aafabce71433fda4f",
+    "url": "/PuzzleGameDeluxe/static/js/runtime-main.8a4c08b7.js"
+  }
+]);
